8382135295417b43e07b086e7a8c2aa0c38aebb0
mohammed anwar <moanbm123@gmail.com>
2024-02-05 16:02:39 +0530

    create init store app

---
+++ android/app/src/main/kotlin/com/moanbm123/store_api/store_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moanbm123.store_api.store_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/mooanbm123/store_api/store_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mooanbm123.store_api.store_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
